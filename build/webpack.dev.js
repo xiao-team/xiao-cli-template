6a8e2372e3de3822f579c28c39a68cfccbdaf866
郑晓鹏 <zxpscau@163.com>
2020-06-10 14:49:08 +0800

    feat: 增加dev相关的配置

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,15 +1,34 @@
 // webpack.dev.js
 // 存放 dev 配置
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const merge = require('webpack-merge');
-const common = require('./webpack.base.js');
-const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const common = require('./webpack.base.js')
+const path = require('path')
+const open = require('opn') //打开浏览器
+const chalk = require('chalk') // 改变命令行中输出日志颜色插件
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
         // 开发服务器
         contentBase: '../dist',
+        host: '0.0.0.0',
+        overlay: true,
+        stats: 'errors-only',
+        after() {
+            open('http://localhost:' + this.port)
+                .then(() => {
+                    console.log(
+                        chalk.cyan(
+                            '成功打开链接： http://localhost:' + this.port
+                        )
+                    )
+                })
+                .catch(err => {
+                    console.log(chalk.red(err))
+                })
+        },
         historyApiFallback: {
             rewrites: [{ from: /./, to: '/index.html' }],
         },
@@ -26,6 +45,7 @@ module.exports = merge(common, {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../index.html'),
         }),
+        new FriendlyErrorsWebpackPlugin(),
     ],
     mode: 'development',
-});
+})
